4703a0d5535159754281cf57936cf1e30f5d3000
Daniel Danis <daniel.danis@jax.org>
2020-06-04 10:59:30 -0400

    Simplify `HtmlResultWriter`

--- threes-cli/src/main/java/org/monarchinitiative/threes/cli/cmd/analyze_vcf/HtmlResultWriter.java
+++ threes-cli/src/main/java/org/monarchinitiative/threes/cli/cmd/analyze_vcf/HtmlResultWriter.java
@@ -25,38 +25,25 @@ public class HtmlResultWriter {
 
     public HtmlResultWriter() {
         Locale.setDefault(Locale.US);
-        templateEngine = getTemplateEngine();
-    }
-
-    private static TemplateEngine getTemplateEngine() {
         ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
         templateResolver.setTemplateMode(TemplateMode.HTML);
         templateResolver.setPrefix("templates/");
         templateResolver.setSuffix(".html");
         templateResolver.setCacheable(true);
 
-        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine = new TemplateEngine();
         templateEngine.setTemplateResolver(templateResolver);
-
-        return templateEngine;
-    }
-
-    private Context buildContext(AnalysisResults results) {
-        Context context = new Context();
-        context.setVariable("sampleName", String.join(", ", results.getSampleNames()));
-
-        context.setVariable("settings", results.getSettingsData());
-
-        context.setVariable("stats", results.getAnalysisStats());
-
-        context.setVariable("variants", results.getVariantData());
-
-        return context;
     }
 
     public void writeResults(OutputStream os, AnalysisResults results) throws IOException {
         try (Writer writer = new OutputStreamWriter(os)) {
-            final Context context = buildContext(results);
+
+            final Context context = new Context();
+            context.setVariable("sampleName", String.join(", ", results.getSampleNames()));
+            context.setVariable("settings", results.getSettingsData());
+            context.setVariable("stats", results.getAnalysisStats());
+            context.setVariable("variants", results.getVariantData());
+
             templateEngine.process("results", context, writer);
         }
     }
