46288bbd54205edbc74ca480afe3f8dfb32e9c10
Daniel Danis <daniel.danis@jax.org>
2020-10-06 23:34:29 +0200

    Use correct track names in splicing annotators

--- squirls-core/src/main/java/org/monarchinitiative/squirls/core/scoring/AbstractSplicingAnnotator.java
+++ squirls-core/src/main/java/org/monarchinitiative/squirls/core/scoring/AbstractSplicingAnnotator.java
@@ -13,6 +13,12 @@ import java.util.Map;
 
 abstract class AbstractSplicingAnnotator implements SplicingAnnotator {
 
+    /**
+     * These have to match with whatever is used by {@link org.monarchinitiative.squirls.core.data.SplicingAnnotationData}
+     */
+    private static final String FASTA_TRACK_NAME = "fasta";
+    private static final String PHYLOP_TRACK_NAME = "phylop";
+
     private final SplicingTranscriptLocator locator;
     private final Map<String, FeatureCalculator> calculatorMap;
 
@@ -25,12 +31,12 @@ abstract class AbstractSplicingAnnotator implements SplicingAnnotator {
     public <T extends Annotatable> T annotate(T data) {
         final GenomeVariant variant = data.getVariant();
         final SplicingTranscript transcript = data.getTranscript();
-        final SequenceRegion seq = data.getTrack("sequence", SequenceRegion.class);
+        final SequenceRegion seq = data.getTrack(FASTA_TRACK_NAME, SequenceRegion.class);
         final SequenceInterval sequence = SequenceInterval.builder()
                 .interval(seq.getInterval())
                 .sequence(seq.getValue())
                 .build();
-        final FloatRegion phylop = data.getTrack("phylop", FloatRegion.class);
+        final FloatRegion phylop = data.getTrack(PHYLOP_TRACK_NAME, FloatRegion.class);
 
         final GenomeVariant variantOnStrand = variant.withStrand(transcript.getStrand());
 
