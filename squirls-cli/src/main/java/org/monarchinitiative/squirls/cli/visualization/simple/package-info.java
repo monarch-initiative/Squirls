d083873bfc068272b913ec136f86e33ec2a39748
Daniel Danis <daniel.danis@jax.org>
2020-11-18 10:15:07 +0100

    Remove simple graphics generator

--- squirls-cli/src/main/java/org/monarchinitiative/squirls/cli/visualization/simple/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This package defines {@link org.monarchinitiative.squirls.cli.visualization.simple.SimpleSplicingVariantGraphicsGenerator}
- * that generates graphics for predicted splicing-pathogenic based on the splicing features.
- */
-package org.monarchinitiative.squirls.cli.visualization.simple;
